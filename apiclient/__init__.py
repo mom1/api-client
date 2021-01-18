8947e5cfd47c7eae11898418d15d4fb53f63be92
MikeWooster <mike.wooster@growthstreet.co.uk>
2021-01-18 15:19:02 +0000

    remove yaml response handler

--- apiclient/__init__.py
+++ apiclient/__init__.py
@@ -9,10 +9,5 @@ from apiclient.client import APIClient
 from apiclient.decorates import endpoint
 from apiclient.paginators import paginated
 from apiclient.request_formatters import JsonRequestFormatter
-from apiclient.response_handlers import (
-    JsonResponseHandler,
-    RequestsResponseHandler,
-    XmlResponseHandler,
-    YamlResponseHandler,
-)
+from apiclient.response_handlers import JsonResponseHandler, RequestsResponseHandler, XmlResponseHandler
 from apiclient.retrying import retry_request
